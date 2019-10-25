316be03e1e5dc03ab76aaf918668fab9f7fef923
owinolawrence <owinolawrence21@gmail.com>
2019-10-25 11:33:25 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QouteComponent } from './qoute/qoute.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QouteComponent
   ],
   imports: [
     BrowserModule,
