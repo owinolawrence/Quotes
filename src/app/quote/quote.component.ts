e75dbb75eebd8eddb5894190d1dad64ccaaa1775
owinolawrence <owinolawrence21@gmail.com>
2019-10-26 14:12:09 +0300

    add addnewquote method

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,10 +12,8 @@ export class QuoteComponent implements OnInit {
 
   quotes: Quotes[] = [
     new Quotes("Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.", "Norman Vincent Peale", "ghjg", new Date(1980, 2, 14)),
-    new Quotes(" Where there is a will, there is a way.If you can dream it you can do it.", "Walt Disney", "ggg", new Date(1990, 5, 16)),
     new Quotes("Press forward. Do not stop do not linger in your journey but strive for the mark set before you.", "George Whitefield", "pppp", new Date(1880, 7, 21)),
     new Quotes("The future belongs to those who believe in the beauty of their dreams ", "Eleanor Roosevelt", "oooo", new Date(1970, 9, 15)),
-    new Quotes("Aim for the moon. If you miss, you may hit a star.”  ", "Clement Stone", "igdfufsdg", new Date(1854, 1, 29)),
     new Quotes("Don’t watch the clock; do what it does. Keep going.", "Sam Levenson", "uisgaifdg", new Date(1990, 6, 19))
   ];
   toggleDetails(index) {
@@ -33,6 +31,12 @@ export class QuoteComponent implements OnInit {
         this.quotes.splice(index, 1);
       }
     }
+  }
+  addNewQuote(quote){
+    let goalLength = this.quotes.length;
+    quote.id = goalLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
   constructor() { }
 
