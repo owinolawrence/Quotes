813aa088364b4dab594e8daaabd5d118f13a9c14
owinolawrence <owinolawrence21@gmail.com>
2019-10-27 00:17:16 +0300

    introduce quote submiter names

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,10 +12,10 @@ export class QuoteComponent implements OnInit {
 
 
   quotes: Quotes[] = [
-    new Quotes("Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.", "Norman Vincent Peale", "", new Date(1980, 2, 14)),
-    new Quotes("Press forward. Do not stop do not linger in your journey but strive for the mark set before you.", "George Whitefield", "", new Date(1880, 7, 21)),
-    new Quotes("The future belongs to those who believe in the beauty of their dreams ", "Eleanor Roosevelt", "oooo", new Date(1970, 9, 15)),
-    new Quotes("Don’t watch the clock; do what it does. Keep going.", "Sam Levenson", "", new Date(1990, 6, 19))
+    new Quotes("Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.", "Norman Vincent Peale", "Owino Lawrence", new Date(1980, 2, 14)),
+    new Quotes("Press forward. Do not stop do not linger in your journey but strive for the mark set before you.", "George Whitefield", "Paul Cheche", new Date(1880, 7, 21)),
+    new Quotes("The future belongs to those who believe in the beauty of their dreams ", "Eleanor Roosevelt", "Roman Reign", new Date(1970, 9, 15)),
+    new Quotes("Don’t watch the clock; do what it does. Keep going.", "Sam Levenson", "Seth Rollens", new Date(1990, 6, 19))
   ];
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
