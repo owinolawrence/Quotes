c77ad96504a442e0b4f6df6f6c64f2d841b00e99
owinolawrence <owinolawrence21@gmail.com>
2019-10-26 10:28:56 +0300

    add a hostlistener decorator

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,16 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) {}
+  @HostListener("click") onclick(){
+    this.textDeco("uppercase")
+  }
+
+private textDeco(action:string)
+  { this.elem.nativeElement.style.textDecoration ="action"}
 
 }
