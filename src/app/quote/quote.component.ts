047d48b56184155dac304d99246f75d0e152ed60
owinolawrence <owinolawrence21@gmail.com>
2019-10-27 14:14:31 +0300

    reduce the number of quote to fit the page size

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -14,13 +14,13 @@ export class QuoteComponent implements OnInit {
   quotes: Quotes[] = [
     new Quotes("Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.", "Norman Vincent Peale", "Owino Lawrence", new Date(1980, 2, 14)),
     new Quotes("Press forward. Do not stop do not linger in your journey but strive for the mark set before you.", "George Whitefield", "Paul Cheche", new Date(1880, 7, 21)),
-    new Quotes("The future belongs to those who believe in the beauty of their dreams ", "Eleanor Roosevelt", "Roman Reign", new Date(1970, 9, 15)),
+    
     new Quotes("Don’t watch the clock; do what it does. Keep going.", "Sam Levenson", "Seth Rollens", new Date(1990, 6, 19))
   ];
   toggleDetails(index) {
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
-  deleteGoal(isComplete, index) {
+  deleteQuote(isComplete, index) {
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].quote}?`)
       if (toDelete) {
