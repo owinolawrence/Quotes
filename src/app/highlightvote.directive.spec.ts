3323063104a2b8706afbad384a18fb570edf0926
owinolawrence <owinolawrence21@gmail.com>
2019-10-28 23:38:16 +0300

    comment content

--- src/app/highlightvote.directive.spec.ts
+++ src/app/highlightvote.directive.spec.ts
@@ -1,9 +1,9 @@
 
-import { HighlightvoteDirective } from './highlightvote.directive';
+// import { HighlightvoteDirective } from './highlightvote.directive';
 
-describe('HighlightvoteDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightvoteDirective ();
-    expect(directive).toBeTruthy();
-  });
-});
+// describe('HighlightvoteDirective', () => {
+//   it('should create an instance', () => {
+//     const directive = new HighlightvoteDirective (value);
+//     expect(directive).toBeTruthy();
+//   });
+// });
